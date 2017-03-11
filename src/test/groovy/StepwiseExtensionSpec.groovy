f66c5608cf88522a05098a72bc5975724f628ed8
Andrii <aabrasha@gmail.com>
2017-03-11 13:44:18 +0200

    - replace full class name with import - use modern javadoc tags

--- src/test/groovy/StepwiseExtensionSpec.groovy
+++ src/test/groovy/StepwiseExtensionSpec.groovy
@@ -15,13 +15,13 @@
 import spock.lang.*
 
 /**
- * Demonstrates how &#64;Stepwise causes a spec to be run in incremental steps.
- * Change a step's condition from <tt>true</tt> to <tt>false</tt>, and observe
+ * Demonstrates how {@code @Stepwise} causes a spec to be run in incremental steps.
+ * Change a step's condition from {@literal true} to {@literal false}, and observe
  * how the remaining steps will be skipped automatically on the next run.
  * Also notice that if you run a single step (e.g. from the IDE's context menu),
  * all prior steps will also be run.
  *
- * <p><tt>&#64;Stepwise</tt> is particularly useful for higher-level specs whose
+ * <p>{@code @Stepwise} is particularly useful for higher-level specs whose
  * methods have logical dependencies.
  *
  * @since 0.4
